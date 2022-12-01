5bed02b59e1695fbe6470b9623a09a1af0417c86
mxcezl <54847071+mxcezl@users.noreply.github.com>
2022-12-01 23:58:48 +0100

    Ajout curseur

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// disable scroll with mouse wheel
+    $(document).on("wheel", "input[type=number]", function(e) {
+        $(this).blur();
+    });
+
+$("#boutonCommencer").click(function(){
+    // add css property to html
+    $("html").css("cursor", "none");
+
+    // css when over html tag
+    $("*").hover(function(){
+        $(this).css("cursor", "none");
+    });
+
+}); 
